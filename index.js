cdcea3fc0c248350877f3a6e3ce7bc86cdb610a2
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2020-02-10 14:46:20 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+
+const app = express();
+
+const { PORT = 8000 } = process.env;
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "keep playing around me"
+  });
+});
+
+
+app.post('/konnektive/webhook/shipping', handleFulfillmentStatusWebhook);
+
+async function handleFulfillmentStatusWebhook(req, res) {
+  try {
+    console.log(new Date().toLocaleTimeString());
+    console.log("requesst arrived");
+    console.log("orders body \n");
+    console.log(JSON.parse(JSON.stringify(req.body)));
+    console.log("orders headers \n");
+    console.log(JSON.parse(JSON.stringify(req.headers)));
+    let { fulfillmentStatus, orderStatus } = req.body;
+    console.log(fulfillmentStatus, orderStatus);
+    if (fulfillmentStatus === "shipped") {
+      //execute your code here
+    }
+    return res.status(200).send("ok");
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+}
+
+app.listen(PORT, function() {
+  console.log("Listening on port :", PORT);
+});
